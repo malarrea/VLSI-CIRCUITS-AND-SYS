ca9557d5e881852921a6c1d3799eec5a86339d68
Logan Campbell <lc002@email.uark.edu>
2018-12-05 10:13:48 -0600

    postexp

--- VLSI PROJECT/stringtest.cpp
+++ VLSI PROJECT/stringtest.cpp
@@ -22,10 +22,11 @@ node *newNode(char n)
 //https://www.youtube.com/watch?v=rQ42CyfJ5EA
 int main()
 {
-  string test = "2 1 H 6 7 V 4 5 V H 3 H V\0";
+  string test = "4 2 1 H 3 H V 5 V\0";
 	vector<char> test1;
   stack<char>evaluate;
   bool start = false;
+  bool flip;
   // WE NEED TO USE POST EXPRESSION STRATEGY
 	for (int position = 0; position < test.size(); position++)
 	{
@@ -35,20 +36,41 @@ int main()
       if ( (test[position] == 'H'|| test[position] == 'V') && (start == false) )
       {
         start = true;
+        flip = true;
         node* netlist = newNode (test[position]);
         while(!evaluate.empty())
         {
-          
+          if (flip == true)
+          {
+            cout << "bottom -> ";
+            flip = false;
+          }
+          else
+          {
+            cout << "top -> ";
+            flip = true;
+          }
+
           cout << evaluate.top() << " ";
           evaluate.pop();
-
         }
       }
       if (test[position] == 'H' && start == true)
       {
         //cout << "H: " << test[position] << endl;
+        flip = true;
         while(!evaluate.empty())
         {
+          if (flip == true)
+          {
+            cout << "bottom -> ";
+            flip = false;
+          }
+          else
+          {
+            cout << "top -> ";
+            flip = true;
+          }
           cout << evaluate.top() << " ";
           evaluate.pop();
 
@@ -58,8 +80,19 @@ int main()
       if (test[position] == 'V' && start == true)
       {
         //cout << "V: " << test[position] << endl;
+        flip = true;
         while(!evaluate.empty())
         {
+          if (flip == true)
+          {
+            cout << "left -> ";
+            flip = false;
+          }
+          else
+          {
+            cout << "right -> ";
+            flip = true;
+          }
           cout << evaluate.top() << " ";
           evaluate.pop();
 
