08b1be2fec4b84ddcaafd076e953f0c1c52c19c5
Logan Campbell <lc002@email.uark.edu>
2018-12-05 00:58:43 -0600

    stringtest.cpp uses postexp

--- VLSI PROJECT/stringtest.cpp
+++ VLSI PROJECT/stringtest.cpp
@@ -6,26 +6,73 @@
 #include <vector>
 using namespace std;
 
+struct node
+{
+	char info;
+	node *left,*right;
+};
 
+node *newNode(char n)
+{
+	node *ptr=new node;
+	ptr->info=n;
+	ptr->left=ptr->right=NULL;
+	return ptr;
+}
 //https://www.youtube.com/watch?v=rQ42CyfJ5EA
 int main()
 {
-  string test = "1 6 H 3 5 V 2 H V 7 4 H V\0";
+  string test = "2 1 H 6 7 V 4 5 V H 3 H V\0";
 	vector<char> test1;
-
-	for (int x = 0; x < test.size(); x++)
+  stack<char>evaluate;
+  bool start = false;
+  // WE NEED TO USE POST EXPRESSION STRATEGY
+	for (int position = 0; position < test.size(); position++)
 	{
-		if (test[x] != '\n' && test[x] != ' ' && test[x] != '\0')
+
+		if (test[position] != '\n' && test[position] != ' ' && test[position] != '\0')
 		{
-			test1.push_back(test[x]);
+      if ( (test[position] == 'H'|| test[position] == 'V') && (start == false) )
+      {
+        start = true;
+        node* netlist = newNode (test[position]);
+        while(!evaluate.empty())
+        {
+          
+          cout << evaluate.top() << " ";
+          evaluate.pop();
+
+        }
+      }
+      if (test[position] == 'H' && start == true)
+      {
+        //cout << "H: " << test[position] << endl;
+        while(!evaluate.empty())
+        {
+          cout << evaluate.top() << " ";
+          evaluate.pop();
+
+        }
+        cout << " : H" << endl;
+      }
+      if (test[position] == 'V' && start == true)
+      {
+        //cout << "V: " << test[position] << endl;
+        while(!evaluate.empty())
+        {
+          cout << evaluate.top() << " ";
+          evaluate.pop();
+
+        }
+        cout<< " : V" << endl;
+      }
+      if (isdigit(test[position]))
+      {
+        //cout << "NUMBER : " << test[position] << endl;
+        evaluate.push(test[position]);
+      }
 		}
 	}
-
-	for (std::vector<char>::const_iterator i = test1.begin(); i != test1.end(); i++)
-	{
-
-	}
-
 
 
 
