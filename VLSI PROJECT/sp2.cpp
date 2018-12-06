c1e7161581140aec20f5a57e2ac06f8509b21f49
Logan Campbell <lc002@email.uark.edu>
2018-12-06 02:50:30 -0600

    getting tired

--- VLSI PROJECT/sp2.cpp
+++ VLSI PROJECT/sp2.cpp
@@ -6,6 +6,85 @@
 #include <vector>
 using namespace std;
 
+// A directed graph using adjacency list representation
+class Graph
+{
+    int V; // No. of vertices in graph
+    vector<int> *adj; // Pointer to an array containing adjacency lists
+
+    // A recursive function used by printAllPaths()
+    void printAllPathsUtil(int , int , bool [], int [], int &);
+
+public:
+    Graph(int V); // Constructor
+    void addEdge(int u, int v);
+    void printAllPaths(int s, int d);
+};
+
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new vector<int>[V];
+}
+
+void Graph::addEdge(int u, int v)
+{
+    adj[u].push_back(v); // Add v to u’s list.
+}
+
+// Prints all paths from 's' to 'd'
+void Graph::printAllPaths(int s, int d)
+{
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+
+    // Create an array to store paths
+    int *path = new int[V];
+    int path_index = 0; // Initialize path[] as empty
+
+    // Initialize all vertices as not visited
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+
+    // Call the recursive helper function to print all paths
+    printAllPathsUtil(s, d, visited, path, path_index);
+}
+
+// A recursive function to print all paths from 'u' to 'd'.
+// visited[] keeps track of vertices in current path.
+// path[] stores actual vertices and path_index is current
+// index in path[]
+void Graph::printAllPathsUtil(int u, int d, bool visited[],
+                            int path[], int &path_index)
+{
+    // Mark the current node and store it in path[]
+    visited[u] = true;
+    path[path_index] = u;
+    path_index++;
+
+    // If current vertex is same as destination, then print
+    // current path[]
+    if (u == d)
+    {
+        for (int i = 0; i<path_index; i++)
+            cout << path[i] << " ";
+        cout << endl;
+
+    }
+    else // If current vertex is not destination
+    {
+        // Recur for all the vertices adjacent to current vertex
+        vector<int>::iterator i;
+        for (i = adj[u].begin(); i != adj[u].end(); ++i)
+            if (!visited[*i])
+                printAllPathsUtil(*i, d, visited, path, path_index);
+    }
+
+    // Remove current vertex from path[] and mark it as unvisited
+    path_index--;
+    visited[u] = false;
+}
+
 void ROF (vector<int> pV, int &mn, vector<int> nV, vector<int> &collect)
 {
   //  [A]   [A]
@@ -248,40 +327,67 @@ int main()
     }
   }
 
+
     int modules = 8;
-    // for :
     vector<int> collect1;
     vector<int> collect2;
     vector<int> collect3;
     vector<int> collect4;
+    Graph g(modules + 2);
+    int s = 1000;
+    int t = 1001;
 
-    // int right[modules][modules];
-    // int left [modules][modules];
-    // int above[modules][modules];
-    // int below[modules][modules];
-
-    int x = 2;
-    ROF(evaluate1,x,evaluate2,collect1);
-    LOF(evaluate1,x,evaluate2,collect2);
-    AOF(evaluate1,x,evaluate2,collect3);
-    BOF(evaluate1,x,evaluate2,collect4);
-
-    for (int x = 0; x < collect1.size(); x ++)
+    for (int x = 1; x <= modules; x++)
     {
-      right[2][x] = collect1[x];
-    }
+      //cout << "MODULE [ " << x << "] : " << endl;
+      ROF(evaluate1,x,evaluate2,collect1);
+      LOF(evaluate1,x,evaluate2,collect2);
+      //AOF(evaluate1,x,evaluate2,collect3);
+      // BOF(evaluate1,x,evaluate2,collect4);
 
-    for (int x = 0; x < modules; x++)
-    {
-      cout << right[2][x] << " ";
-    }
-        // mod[x][z] = ROF(evaluate1,x,evaluate2);
-        // mod[x][z] = LOF(evaluate1,x,evaluate2);
-        // mod[x][z] = AOF(evaluate1,x,evaluate2);
-        // mod[x][z] = BOF(evaluate1,x,evaluate2);
-        //cout << endl; cout << endl;
-//:
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+      if (collect1.empty())
+      {
+        //cout << "CONNECTED" << endl;
+        g.addEdge(s,x);
+      }
+      else
+      {
+        while(!collect1.empty())
+        {
+          cout << "CONNECTED" << endl;
+          g.addEdge(collect1.back(),x);
+          collect1.pop_back();
+        }
+      }
+      collect1.clear();
+
+
+
+
+
+
+      if (collect2.empty())
+      {
+        //cout << "CONNECTED" << endl;
+        g.addEdge(x,t);
+      }
+      collect2.clear();
+
+    }
 
   return 0;
 }
